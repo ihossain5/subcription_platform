119acdf6ddb656f419802c2eb27f1e9e6e71af49
ihossain5 <ismail.hossain5169@gmail.com>
2021-09-24 18:16:42 +0600

    validation added

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -12,11 +12,21 @@ class SubscriptionController extends Controller
     public function subscribeWebsite(SubscriberStoreRequest $request){
         $website = Website::find($request->website_id);
         if($website){
-            $store_subscriber = Subscriber::create($request->validated());
-            return response()->json([
-                'status'=> true,
-                'message'=>"You've successfully subscribed the website",
-            ],201);
+            $suscriber = Subscriber::where('website_id', $website->id)
+            ->where('email',$request->email)->first();
+            if($suscriber){
+                return response()->json([
+                    'status'=> false,
+                    'message'=>"You've alerady subscribed this website",
+                ],200);
+            }else{
+                $store_subscriber = Subscriber::create($request->validated());
+                return response()->json([
+                    'status'=> true,
+                    'message'=>"You've successfully subscribed the website",
+                ],201);
+            }
+      
         }else{
             return response()->json([
                 'status'=> false,
